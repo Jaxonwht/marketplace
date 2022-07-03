efae3cae462713dfa0ac7a692e9cf1ed986879f8
Haotian Wang <haotian@duolingo.com>
2022-07-03 17:20:41 -0400

    Add restriction when applying tables

--- web_backend/dal/deal_dal.py
+++ web_backend/dal/deal_dal.py
@@ -1,9 +1,15 @@
 from datetime import datetime
 from typing import Iterable, List, Optional
+from flask import abort
 
 from sqlalchemy import select
 from db import flask_session
 from models.deal_model import Deal
+from models.dealer_model import Dealer
+
+
+# Maximum allowed rate of profit or loss.
+_MAXIMUM_ALLOWED_RATE = 0.2
 
 
 def current_open_deals(max_share_price: Optional[float] = None) -> Iterable[Deal]:
@@ -27,6 +33,25 @@ def create_deal(
     start_time: datetime,
     end_time: datetime,
 ) -> Deal:
+    if not allowed_rates:
+        abort(400, "You must specify non-empty list of allowed rates")
+    for rate in allowed_rates:
+        if rate <= 0 or rate >= _MAXIMUM_ALLOWED_RATE:
+            abort(400, f"The rate {rate} does not fall within the range of (0, {_MAXIMUM_ALLOWED_RATE}]")
+    if share_price <= 0:
+        abort(400, "Share price must be positive")
+    if initial_number_of_shares <= 0:
+        abort(400, "Initial number of shares must be positive")
+    dealer = flask_session.get(Dealer, dealer_name, with_for_update={"key_share": True})
+    if not dealer:
+        abort(404, f"Dealer {dealer_name} not found")
+    amount_needed = (1 + max(allowed_rates)) * share_price * initial_number_of_shares
+    if dealer.lockup_balance + amount_needed >= dealer.balance:
+        abort(
+            409,
+            f"Issuing these shares require a balance of {dealer.lockup_balance + amount_needed}, but the dealer only has {dealer.balacne}",
+        )
+    dealer.lockup_balance = Dealer.lockup_balance + amount_needed
     new_deal = Deal(
         dealer_name=dealer_name,
         nft_id=nft_id,
