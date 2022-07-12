99185f88d365073e8f70b370f3eb5ffde8afdb04
zhoujc999@gmail.com <Jingchao Zhou>
2022-07-12 17:15:04 +0000

    Add flask apscheduler

--- web_backend/app.py
+++ web_backend/app.py
@@ -2,6 +2,8 @@
 from logging import INFO
 from flask import Flask
 
+from flask_apscheduler import APScheduler
+
 
 def create_app() -> Flask:
     """Application factory."""
@@ -11,6 +13,13 @@ def create_app() -> Flask:
     # Default to configs.Config which is the base configuration shared by all environments.
     flask_config = app.config.get("CONFIG", "Config")
     app.config.from_object(f"configs.{flask_config}")
+
+    # Initialize scheduler
+    scheduler = APScheduler()
+    # Set options
+    # scheduler.api_enabled = True
+    scheduler.init_app(app)
+    scheduler.start()
 
     app.logger.setLevel(app.config.get("MAIN_LOGGING_LEVEL", INFO))  # pylint: disable=no-member
 
