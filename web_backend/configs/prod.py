5d68e6377dac345edd2f40f12f86f5b6202e6186
Jingchao Zhou <zhoujc999@gmail.com>
2022-07-13 17:24:29 +0000

    Read from secret file instead of env

--- web_backend/configs/prod.py
+++ web_backend/configs/prod.py
@@ -1,6 +1,5 @@
 """Development config."""
-
-import os
+from pathlib import Path
 from logging import INFO
 from configs.base import Config
 
@@ -8,6 +7,6 @@ from configs.base import Config
 class ProductionConfig(Config):  # pylint: disable=too-few-public-methods
     """Configurations used during production."""
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "SQLALCHEMY_DATABASE_URI not specified")
+    SQLALCHEMY_DATABASE_URI = (Path("/etc") / "SQLALCHEMY_DATABASE_URI").read_text()
     SQLALCHEMY_ECHO = False
     MAIN_LOGGING_LEVEL = INFO
