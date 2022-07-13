5b8717a041efcfca1a76c11913d06f3ce9c3dd37
zhoujc999@gmail.com <Jingchao Zhou>
2022-07-13 04:07:26 +0000

    Remove apscheduler

--- apscheduler/configs/docker_dev.py
+++
@@ -1,9 +0,0 @@
-"""Development config specifically for a docker environment."""
-
-from configs.base import Config
-
-
-class DockerDevelopmentConfig(Config):  # pylint: disable=too-few-public-methods
-    """Configurations used during development."""
-
-    SQLALCHEMY_DATABASE_URI = "postgresql://local_user:dev_password@db:5432/local_db"
--- apscheduler/configs/stage.py
+++
@@ -1,9 +0,0 @@
-"""Development config."""
-
-from configs.base import Config
-
-
-class StagingConfig(Config):  # pylint: disable=too-few-public-methods
-    """Configurations used in staging environments."""
-
-    SQLALCHEMY_DATABASE_URI = "postgresql://local_user:dev_password@host.docker.internal:5432/local_db"
