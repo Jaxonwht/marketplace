f4b9e7750b4e45acae005ffa1af7c7f5d23934c0
Haotian Wang <haotian@duolingo.com>
2022-07-13 12:09:01 -0400

    Set up stage and prod dockerfiles

--- web_backend/configs/prod.py
+++ web_backend/configs/prod.py
@@ -1,5 +1,6 @@
 """Development config."""
 
+import os
 from logging import INFO
 from configs.base import Config
 
@@ -7,6 +8,6 @@ from configs.base import Config
 class ProductionConfig(Config):  # pylint: disable=too-few-public-methods
     """Configurations used during production."""
 
-    SQLALCHEMY_DATABASE_URI = "postgresql://local_user:dev_password@host.docker.internal:5432/local_db"
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "SQLALCHEMY_DATABASE_URI not specified")
     SQLALCHEMY_ECHO = False
     MAIN_LOGGING_LEVEL = INFO
