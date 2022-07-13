7d413609516d823e0c699f88adc51257204db66b
zhoujc999@gmail.com <Jingchao Zhou>
2022-07-13 04:07:26 +0000

    Remove apscheduler

--- apscheduler/configs/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Import all the configs here for use."""
-from configs.base import Config  # noqa: F401
-from configs.docker_dev import DockerDevelopmentConfig  # noqa: F401
-from configs.stage import StagingConfig  # noqa: F401
-from configs.prod import ProductionConfig  # noqa: F401
--- apscheduler/configs/prod.py
+++
@@ -1,9 +0,0 @@
-"""Development config."""
-
-from configs.base import Config
-
-
-class ProductionConfig(Config):  # pylint: disable=too-few-public-methods
-    """Configurations used during production."""
-
-    SQLALCHEMY_DATABASE_URI = "postgresql://local_user:dev_password@host.docker.internal:5432/local_db"
