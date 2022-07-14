27fc9fe18d6bfa521ef16413566ba68055a71546
Jingchao Zhou <zhoujc999@gmail.com>
2022-07-14 06:27:48 +0000

    Use kube secret

--- web_backend/configs/prod.py
+++ web_backend/configs/prod.py
@@ -7,6 +7,6 @@ from configs.base import Config
 class ProductionConfig(Config):  # pylint: disable=too-few-public-methods
     """Configurations used during production."""
 
-    SQLALCHEMY_DATABASE_URI = (Path("/etc") / "SQLALCHEMY_DATABASE_URI").read_text()
+    SQLALCHEMY_DATABASE_URI = (Path("/var") / "SQLALCHEMY_DATABASE_URI").read_text()
     SQLALCHEMY_ECHO = False
     MAIN_LOGGING_LEVEL = INFO
--- web_backend/configs/stage.py
+++ web_backend/configs/stage.py
@@ -7,6 +7,6 @@ from configs.base import Config
 class StagingConfig(Config):  # pylint: disable=too-few-public-methods
     """Configurations used in staging environments."""
 
-    SQLALCHEMY_DATABASE_URI = (Path("/etc") / "SQLALCHEMY_DATABASE_URI").read_text()
+    SQLALCHEMY_DATABASE_URI = (Path("/var") / "SQLALCHEMY_DATABASE_URI").read_text()
     SQLALCHEMY_ECHO = True
     MAIN_LOGGING_LEVEL = INFO
