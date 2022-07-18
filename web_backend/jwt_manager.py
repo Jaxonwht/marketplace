5dc7506343589f44c3572f5c5d84cac21387aa66
Haotian Wang <haotian@duolingo.com>
2022-07-18 17:12:26 -0400

    Add authorization bearer auth

--- web_backend/jwt_manager.py
+++ web_backend/jwt_manager.py
@@ -1,3 +1,37 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Dict
 from flask_jwt_extended import JWTManager
 
 jwt = JWTManager()
+
+
+class AccountType(Enum):
+    """Account types supported in marketplace."""
+
+    BUYER = "buyer"
+    DEALER = "dealer"
+
+
+@dataclass
+class MarketplaceIdentity:
+    """The JWT user identity for marketplace."""
+
+    account_type: AccountType
+    username: str
+
+    def as_dict(self) -> Dict[str, str]:
+        """JSON serializable form."""
+        return {"account_type": self.account_type.value, "username": self.username}
+
+
+@jwt.user_identity_loader
+def user_identity_loader(identity: MarketplaceIdentity) -> Dict[str, str]:
+    """Serialized form used in jwt hashing."""
+    return identity.as_dict()
+
+
+@jwt.user_lookup_loader
+def user_lookup_loader(_jwt_header: Dict, jwt_data: Dict) -> MarketplaceIdentity:
+    """Deserialize a jwt key content."""
+    return MarketplaceIdentity(AccountType(jwt_data["sub"]["account_type"]), jwt_data["sub"]["username"])
--- web_backend/views/auth.py
+++ web_backend/views/auth.py
@@ -1,14 +1,17 @@
-from flask import Blueprint, abort, current_app, request
+from datetime import timedelta
+from flask import Blueprint, abort, current_app, jsonify, request
+from flask_jwt_extended import create_access_token, get_current_user, jwt_required
 
 from dal.buyer_dal import get_buyer_by_name
 from dal.dealer_dal import get_dealer_by_name
+from jwt_manager import AccountType, MarketplaceIdentity
 from utils.auth_utils import verify_password
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
 @auth_bp.post("/login")
-def login():
+def login():  # pylint: disable=inconsistent-return-statements
     """
     Log in with a set of user name and password. This login uses the Authorization
     Basic header.
@@ -26,9 +29,12 @@ def login():
     if password is None:
         abort(400, "password is missing in Auth header")
     account_type = request.args.get("account_type")
-    if account_type not in {"buyer", "dealer"}:
+    try:
+        valid_account_type = AccountType(account_type)
+    except ValueError:
         abort(400, "Account type must be either 'dealer' or 'buyer'")
-    if account_type == "buyer":
+    user_identity = MarketplaceIdentity(valid_account_type, username)
+    if valid_account_type == AccountType.BUYER:
         buyer = get_buyer_by_name(username)
         if buyer is None:
             abort(404, f"Buyer {username} is not found")
@@ -36,9 +42,10 @@ def login():
             password, buyer.password_hash, buyer.salt, current_app.config["PASSWORD_HASH_ITERATIONS"]
         )
         if password_match:
-            return "Login successful"
+            access_token = create_access_token(user_identity, expires_delta=timedelta(days=1))
+            return jsonify(access_token=access_token)
         abort(401, "Password does not match")
-    elif account_type == "dealer":
+    elif valid_account_type == AccountType.DEALER:
         dealer = get_dealer_by_name(username)
         if dealer is None:
             abort(404, f"Dealer {username} is not found")
@@ -46,5 +53,17 @@ def login():
             password, dealer.password_hash, dealer.salt, current_app.config["PASSWORD_HASH_ITERATIONS"]
         )
         if password_match:
-            return "Login successful"
+            access_token = create_access_token(user_identity, expires_delta=timedelta(days=1))
+            return jsonify(access_token=access_token)
         abort(401, "Password does not match")
+
+
+@auth_bp.get("/status")
+@jwt_required()
+def login_status():
+    """
+    Only a logged-in user will be able to access this route. This is a convenient route to check whether
+    the request initiater has the appropriate JWT tokens.
+    """
+    identity: MarketplaceIdentity = get_current_user()
+    return jsonify(identity.as_dict())
