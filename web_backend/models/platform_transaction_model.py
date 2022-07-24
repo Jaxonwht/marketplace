d2e81a2306d9d6217af41f7c9959b16a74cc66be
Haotian Wang <haotian@duolingo.com>
2022-07-24 15:15:23 -0400

    Modify primary key of platform_transaction_model

--- web_backend/models/platform_transaction_model.py
+++ web_backend/models/platform_transaction_model.py
@@ -1,7 +1,7 @@
 """Actual transaction with the platform in terms of USDC."""
 from datetime import datetime
 
-from sqlalchemy import Column, Float, ForeignKey, Integer, String
+from sqlalchemy import Column, Float, ForeignKey, String
 from sqlalchemy.dialects.postgresql import TIMESTAMP
 from db import flask_db
 
@@ -9,7 +9,7 @@ from db import flask_db
 class PlatformTransaction(flask_db.Model):
     """Transactions that refer to actual monetary transactions."""
 
-    id = Column(Integer, primary_key=True)
+    transaction_hash = Column(String, primary_key=True)
     amount = Column(Float, nullable=False)
     timestamp = Column(TIMESTAMP, nullable=False, default=datetime.now)
     buyer_name = Column(String, ForeignKey("buyer.name", ondelete="CASCADE"))
