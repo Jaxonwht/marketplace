b62b1d3c3e75581dd139d9a4fae5334ad3422f10
Jingchao Zhou <zhoujc999@gmail.com>
2022-09-23 05:38:34 +0000

    Typography

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,18 +1,30 @@
-import { Link } from "react-router-dom";
+import { Space, Typography } from "antd";
 import React from "react";
 import styles from "./index.module.css";
 import intl from "react-intl-universal";
+
+const { Text, Link, Title } = Typography;
 
 export default function Layout() {
   return (
     <div className={styles.footer}>
       <div className={styles.content}>
-        <div className={styles.left}>CONTACT INFO</div>
+        <div className={styles.left}>
+          <Title level={3}>CONTACT INFO</Title>
+        </div>
         <div className={styles.right}>
           <div className={styles.rightItem}>
-            Email: masterSunDoctorNai@unicleChao.com
+            <Text> Email: </Text>
+            <Link
+              href="mailto:masterSunDoctorNai@unicleChao.com"
+              target="_blank"
+            >
+              masterSunDoctorNai@unicleChao.com
+            </Link>
           </div>
-          <div className={styles.rightItem}>phone: (999)999-9999</div>
+          <div className={styles.rightItem}>
+            <Text>Phone: (999)999-9999</Text>
+          </div>
         </div>
       </div>
     </div>
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -5,11 +5,11 @@ import { clear, getUser, storeCredentialsIfDev } from "../../utils/storage";
 import MyMenu from "../Menu/Menu";
 
 import { Link, useNavigate } from "react-router-dom";
-import { Dropdown, Menu, Switch } from "antd";
+import { Dropdown, Select, Avatar, Typography } from "antd";
 import intl from "react-intl-universal";
 import { useDispatch } from "react-redux";
 import { isMobile } from "../../utils/utils";
-import { MenuOutlined } from "@ant-design/icons";
+import { MenuOutlined, SearchOutlined } from "@ant-design/icons";
 import { request } from "http";
 import CryptoSignIn from "../../components/metamask/CryptoSignIn";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
@@ -23,8 +23,11 @@ import {
   selectAllNonClosedDealInfo,
 } from "../../selectors/dealInfo";
 import { fetchAllDealInfo } from "../../reduxSlices/dealInfoSlice";
+import { shortenAddress } from "../../utils/address";
 
 const Navbar = () => {
+  const { Text } = Typography;
+  const { Option } = Select;
   const [navBg, setNavBg] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
   const dispatch = useAppDispatch();
@@ -57,13 +60,52 @@ const Navbar = () => {
   const signInDisplay = identity ? (
     <div className={styles["sign-out-container"]}>
       <div className={styles.welcomeMessage}>
-        Welcome, {identity.account_type} {identity.username.substring(0, 8)}****
+        <Text>
+          Welcome, {identity.account_type} {shortenAddress(identity.username)}
+        </Text>
       </div>
       <CryptoSignOut />
     </div>
   ) : (
     <CryptoSignIn />
   );
+
+  const SearchInput: React.FC<{ placeholder: string; style: React.CSSProperties }> = props => {
+  const [data, setData] = useState<any[]>([]);
+  const [value, setValue] = useState<string>();
+
+  const handleSearch = (newValue: string) => {
+    if (newValue) {
+      // fetch(newValue, setData);
+    } else {
+      setData([]);
+    }
+  };
+
+  const handleChange = (newValue: string) => {
+    setValue(newValue);
+  };
+
+  const options = data.map(d => <Option key={d.value}>{d.text}</Option>);
+
+  return (
+    <Select
+      showSearch
+      value={value}
+      placeholder={props.placeholder}
+      style={props.style}
+      defaultActiveFirstOption={false}
+      showArrow={false}
+      filterOption={false}
+      onSearch={handleSearch}
+      onChange={handleChange}
+      notFoundContent={null}
+    >
+      {options}
+    </Select>
+  );
+};
+
 
   const [searchStr, setSearchStr] = useState<string>("");
 
@@ -75,36 +117,16 @@ const Navbar = () => {
       <div className={styles.headerContent}>
         <Link to={"/home"}>
           <div className={styles.caption} style={{ cursor: "pointer" }}>
-            <img
+            <Avatar
               alt=""
               className={styles.logo}
               src={require("../../assets/images/logo.jpg")}
-            ></img>
-            IIASS
+            ></Avatar>
+            <Text>AISSI</Text>
           </div>
         </Link>
-        <div className={styles.searchInput}>
-          <img
-            style={{ width: 20, height: 20 }}
-            src={require("../../assets/images/search.png")}
-            alt=""
-          ></img>
-          <input
-            placeholder="Search Bar"
-            value={searchStr}
-            onChange={(e) => {
-              e.preventDefault();
-              setSearchStr(e.target.value);
-            }}
-            onKeyUp={(e) => {
-              if (e.keyCode === 13) {
-                dispatch(fetchAllDealInfo);
-                // request()
-              }
-            }}
-          ></input>
-        </div>
-        <div style={{ flex: 1 }}>
+          <SearchInput placeholder="Search" style={{ width: 200 }}/>
+          <div style={{ flex: 1 }} >
           <div className={styles.menus}>
             <MyMenu></MyMenu>
           </div>
